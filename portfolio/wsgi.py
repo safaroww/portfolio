d69f47c27cac1cf567ec0bee0ef579f615335529
safaroww <asifseferov00@gmail.com>
2022-08-09 19:37:25 +0400

    some problems fixed

--- portfolio/wsgi.py
+++ portfolio/wsgi.py
@@ -8,9 +8,11 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
+from dotenv import load_dotenv
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'portfolio.settings')
+load_dotenv()
 
 application = get_wsgi_application()
