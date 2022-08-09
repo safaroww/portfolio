57a595ca4060a7b569d1a42abed8cc79ee4deb15
safaroww <asifseferov00@gmail.com>
2022-08-09 19:27:25 +0400

    settings fixed

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'staticfiles/'
+STATIC_ROOT = 'staticfiles'
 
 STATICFILES_DIRS = [
     'static'
